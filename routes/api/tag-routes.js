33e3922d8d8a7b7bfc2ebdcc30d9ba272876fe20
grace butler <gmx.butler@gmail.com>
2021-08-03 18:23:30 -0500

    GET tags by id working now

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }],
+      include: { model: Product, attributes: ['product_name', 'price', 'stock', 'category_id']}
     })
 
     if (!tagData) {
